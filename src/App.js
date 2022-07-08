97e27b483709a95a4d1fa2bcef3f09c4f84c0bf9
Zachary Grainge <zgrainge0@gmail.com>
2022-07-08 17:58:59 -0400

    setup time search for time in seconds & setup Genre Search for genreId

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import firebase from './firebase';
 import { ref, getDatabase, onValue } from 'firebase/database'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Search from './Search';
 
 // API-Key c17f9dde6c0743f195a962da663f6626
 
@@ -14,50 +15,52 @@ function App() {
   const database = getDatabase(firebase)
   const dbRef = ref(database)
 
-  useEffect(() => {
-    onValue(dbRef, (snapshot) => {
-      if (snapshot.exists()) {
-        const data = snapshot.val()
-        console.log(data);
-      } else {
-        console.log('No Data Available');
-      }
-    })
+  // useEffect(() => {
+  //   onValue(dbRef, (snapshot) => {
+  //     if (snapshot.exists()) {
+  //       const data = snapshot.val()
+  //       console.log(data);
+  //     } else {
+  //       console.log('No Data Available');
+  //     }
+  //   })
 
 
-  }, [])
-  useEffect(() => {
-    axios({
-      url: 'https://listen-api-test.listennotes.com/api/v2/genres',
-      // headers: {
-      //   'X-ListenAPI-Key': 'c17f9dde6c0743f195a962da663f6626'
-      // },
-      params: {
-        top_level_only: 1,
-      }
-    }).then((response) => {
-      console.log(response);
-    })
-  }, [])
+  // }, [])
+  // useEffect(() => {
+  //   axios({
+  //     url: 'https://listen-api-test.listennotes.com/api/v2/genres',
+  //     // headers: {
+  //     //   'X-ListenAPI-Key': 'c17f9dde6c0743f195a962da663f6626'
+  //     // },
+  //     params: {
+  //       top_level_only: 1,
+  //     }
+  //   }).then((response) => {
+  //     console.log(response);
+  //   })
+  // }, [])
 
-  useEffect(() => {
-    axios({
-      url: 'https://listen-api-test.listennotes.com/api/v2/best_podcasts',
-      // headers: {
-      //   'X-ListenAPI-Key': 'c17f9dde6c0743f195a962da663f6626'
-      // },
-      params: {
-        gener_id: 133,
-        page: 1,
-      }
-    }).then((response) => {
-      console.log(response);
-    })
-  }, [])
+  // useEffect(() => {
+  //   axios({
+  //     url: 'https://listen-api-test.listennotes.com/api/v2/best_podcasts',
+  //     // headers: {
+  //     //   'X-ListenAPI-Key': 'c17f9dde6c0743f195a962da663f6626'
+  //     // },
+  //     params: {
+  //       gener_id: 133,
+  //       page: 1,
+  //     }
+  //   }).then((response) => {
+  //     console.log(response);
+  //   })
+  // }, [])
 
   return (
-    <h1>Talkie Walkie 😊</h1>
-
+    <>
+      <h1>Talkie Walkie 😊</h1>
+      <Search />
+    </>
   );
 }
 
